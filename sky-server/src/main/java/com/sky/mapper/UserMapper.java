fb954101731ba309259259713aa8b169381c9c72
c9noo <c9noo@aliyun.com>
2023-09-24 16:57:34 +0800

    微信订单支付（因没有营业执照 所以修改了部分代码 进行实现支付）

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -18,4 +18,7 @@ public interface UserMapper {
     User getByOpenId(String openid);
 
     void insert(User user);
+
+    @Select("select * from user where id = #{userId}" )
+    User getById(Long userId);
 }
